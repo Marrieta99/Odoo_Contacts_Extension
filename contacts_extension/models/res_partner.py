e6564cb20d2fa87f5ddbead88ea8d5d2721552f5
Marrieta99 <arrieta.maxi@gmail.com>
2024-03-05 09:54:14 -0300

    agregar traduccion

--- contacts_extension/models/res_partner.py
+++ contacts_extension/models/res_partner.py
@@ -5,6 +5,8 @@ class Partner(models.Model):
     _description = "Contact"
     _inherit = "res.partner"
 
+    industry_id = fields.Many2one('res.partner.industry', 'Category')
+
     segment_id = fields.Many2one(
         string="Segmento",
         comodel_name="segmento"
